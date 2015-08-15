d0ab8aa506f197e018b6a16f3bcf753b9c219433
Anthony Autrey <flameofprometheus@gmail.com>
2015-08-14 21:34:44 -0500

    added max/min height parameter to base layer, now ignoring temp directory

--- Assets/CreateNewMeshFidelity.cs
+++ Assets/CreateNewMeshFidelity.cs
@@ -45,13 +45,13 @@ public class CreateNewMeshFidelity : MonoBehaviour
 				int iterations = (int)(chunkSize*fidelity);
 	
 
-				TerrainPerlin land = new TerrainPerlin (2f, 0f, 9999, 0, new Color(.75f, .50f, .50f, 1), new Color(0, 1, 0, 1));
+				TerrainPerlin land = new TerrainPerlin (2f, 0f, 9999, 0, new Color(0f, 1f, 0f, 0.5f), new Color(0, 1, 0, 1));
 				
-				land.addLayer (20, 100, 999, 0, 60, new Color(0, 1, 0, 1), new Color(0, 1, 0, 1));
+				//land.addLayer (20, 100, 999, 0, 60, new Color(0, 1, 0, 1), new Color(0, 1, 0, 1));
 				land.addLayer (75, 50, 999, 0, 25, new Color(1, 0, 0, 1), new Color(0, 1, 0, 1));
-				land.addLayer (70, 150, 999, 0, 95, new Color(0, 0, 1, 1), new Color(0, 1, 0, 1));	
-				land.addPass (2, 2f, 5f, 999, 0);
-				//land.addPass (0, 2, 7f, 0, 6);
+				land.addLayer (70, 200, 25, 15, 100, new Color(0, 0, 1, 1), new Color(0, 1, 0, 1));	
+				//land.addPass (2, 2f, 5f, 999, 0);
+				//land.addPass (3, 2, 17f, 0, 0);
 				//land.addPass (1, 7f, 8, 0, 0);
 				//land.addPass (0, 10, 2, 0, 0);
 
--- Assets/TerrainPerlin.cs
+++ Assets/TerrainPerlin.cs
@@ -1,122 +1,125 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-//add a random seed later
+//TO DO
+
+//Comment this shit
+//add max/min functionality to passes
+//add a random seed to noise
 //add negative passes and layers
 //allow some passes to affect all layers
 //allow some layers to affect only specific layers
-//send back color info
-//implement min and max
+//implement second color
+// allow offset with passes for sparse effect
+// allow colors with passes (make additional constructor with colors as arguments
 
 
 //probably going to have to produce whole mesh here
 //send in starting coordinates and chunk size, send back entire colored mesh
 
-public class TerrainPerlin : MonoBehaviour {
-	private List<List<float>> scale = new List<List<float>>();
-	private List<List<float>> height= new List<List<float>>();
-	private List<List<float>> heightMax= new List<List<float>>();
-	private List<List<float>> heightMin= new List<List<float>>();
-	private List<float> offset = new List<float>();
+public class TerrainPerlin : MonoBehaviour
+{
+	private List<List<float>> scale = new List<List<float>> ();
+	private List<List<float>> height = new List<List<float>> ();
+	private List<List<float>> heightMax = new List<List<float>> ();
+	private List<List<float>> heightMin = new List<List<float>> ();
+	private List<float> offset = new List<float> ();
 	private int brownianOctaves = 4;
-	private List<Color> colorOne = new List<Color>();
+	private List<Color> colorOne = new List<Color> ();
 	private List<Color> globalColor = new List<Color> ();
 	public List<float> noiseMax = new List<float> ();
 	public List<float> noiseMin = new List<float> ();
 
-public List<Color> getGlobalColor(){
+	public List<Color> getGlobalColor ()
+	{
 		return globalColor;
-		}
-
-public TerrainPerlin(float scale, float height, float heightMax, float heightMin, Color colorOne, Color colorTwo){
-
-	this.scale.Add (new List<float>());
-	this.scale[0].Add(scale);
-	
-	this.height.Add (new List<float>());
-	this.height[0].Add(height);
-
-	this.heightMax.Add (new List<float>());
-	this.heightMax[0].Add(heightMax);
-
-	this.heightMin.Add (new List<float>());
-	this.heightMin[0].Add(heightMin);
-
-	this.colorOne.Add(colorOne);
-
-	this.offset.Add(0);
 	}
 
+	public TerrainPerlin (float scale, float height, float heightMax, float heightMin, Color colorOne, Color colorTwo)
+	{
 
-public void addPass(int layer, float scale, float height, float heightMax, float heightMin){
-	this.scale[layer].Add(scale);
-	this.height[layer].Add(height);
-	this.heightMax[layer].Add(heightMax);
-	this.heightMin[layer].Add(heightMin);
+		this.scale.Add (new List<float> ());
+		this.scale [0].Add (scale);
+	
+		this.height.Add (new List<float> ());
+		this.height [0].Add (height);
+
+		this.heightMax.Add (new List<float> ());
+		this.heightMax [0].Add (heightMax);
+
+		this.heightMin.Add (new List<float> ());
+		this.heightMin [0].Add (heightMin);
+
+		this.colorOne.Add (colorOne);
+
+		this.offset.Add (0);
 	}
 
-	public void addLayer(float scale, float height, float heightMax, float heightMin, float offset, Color colorOne, Color colorTwo){
-	this.scale.Add(new List<float>());
-	this.height.Add(new List<float>());
-	this.heightMax.Add(new List<float>());
-	this.heightMin.Add(new List<float>());
+	public void addPass (int layer, float scale, float height, float heightMax, float heightMin)
+	{
+		this.scale [layer].Add (scale);
+		this.height [layer].Add (height);
+		this.heightMax [layer].Add (heightMax);
+		this.heightMin [layer].Add (heightMin);
+	}
 
-	this.scale[this.scale.Count-1].Add(scale);
-	this.height[this.height.Count-1].Add(height);
-	this.heightMax[this.heightMax.Count-1].Add (heightMax);
-	this.heightMin[this.heightMin.Count-1].Add (heightMin);
+	public void addLayer (float scale, float height, float heightMax, float heightMin, float offset, Color colorOne, Color colorTwo)
+	{
+		this.scale.Add (new List<float> ());
+		this.height.Add (new List<float> ());
+		this.heightMax.Add (new List<float> ());
+		this.heightMin.Add (new List<float> ());
 
-	this.colorOne.Add(colorOne);
-	this.noiseMax.Add (-99999);
-	this.noiseMin.Add (99999);
+		this.scale [this.scale.Count - 1].Add (scale);
+		this.height [this.height.Count - 1].Add (height);
+		this.heightMax [this.heightMax.Count - 1].Add (heightMax);
+		this.heightMin [this.heightMin.Count - 1].Add (heightMin);
 
-	this.offset.Add(offset);
+		this.colorOne.Add (colorOne);
+		this.noiseMax.Add (-99999);
+		this.noiseMin.Add (99999);
+
+		this.offset.Add (offset);
 	
-}
-public float getNoise(float x, float z, bool colorizeVertex){
-		float[] origHeight = new float[scale.Count+1];
-		//origHeight[0] = (brownianNoise(x/scale[0][0], z/scale[0][0])*height[0][0]);
-		Color finalColor = colorOne[0];
+	}
+
+	public float getNoise (float x, float z, bool colorizeVertex)
+	{
+		float[] origHeight = new float[scale.Count + 1];
+		Color finalColor = colorOne [0];
 
 		float finalHeight = 0;
 		float minusScale = 0;
 
-		for (int layer = 0;  layer < scale.Count ; layer++){
-			origHeight[layer] = (brownianNoise(x/scale[layer][0], z/scale[layer][0])*height[layer][0]) - offset[layer];
-			for (int pass = 1;  pass < scale[layer].Count ; pass++){
-				float noise = (brownianNoise(x/scale[layer][pass], z/scale[layer][pass])*height[layer][pass]);
-				origHeight[layer] += noise;
-				//origHeight[layer] += Mathf.Min(noise, heightMin[layer][pass]);
-				minusScale += scale[layer][pass];
+		for (int layer = 0; layer < scale.Count; layer++) {
+			origHeight [layer] = (brownianNoise (x / scale [layer] [0], z / scale [layer] [0]) * height [layer] [0]) - offset [layer];
+			if (origHeight [layer] > heightMax [layer] [0]) {
+				origHeight [layer] = heightMax [layer] [0];
 			}
 
-			//finalHeight = Mathf.Max(finalHeight , origHeight[layer]);
-
-			if (finalHeight > origHeight[layer] && layer > 0){
-				//finalColor = colorOne[layer - 1];
-			}
-			else {
-				finalHeight = origHeight[layer];
-				//if (layer > 0) {
-					finalColor = colorOne[layer];//colorOne[layer-1];
-				//}
+			for (int pass = 1; pass < scale[layer].Count; pass++) {
+				float noise = (brownianNoise (x / scale [layer] [pass], z / scale [layer] [pass]) * height [layer] [pass]);
+				origHeight [layer] += noise;
+				minusScale += scale [layer] [pass];
 			}
 
-			finalHeight = Mathf.Max(origHeight[layer] , finalHeight);
-
-
-			//if ( finalHeight > noiseMax[layer]) {noiseMax[layer] = finalHeight;}
-			//if ( finalHeight < noiseMin[layer]) {noiseMin[layer] = finalHeight;}
+			if (origHeight [layer] > heightMin [layer] [0] && origHeight[layer] > finalHeight) {
+					finalColor = colorOne [layer];
+					finalHeight = origHeight[layer];
+			}
 
 		}
 
-		if (colorizeVertex) {globalColor.Add (finalColor);}
+		if (colorizeVertex) {
+			globalColor.Add (finalColor);
+		}
 
-		return finalHeight - minusScale/10;
+		return finalHeight - minusScale / 10;
 
 	}
 
-private float brownianNoise(float x, float z){
+	private float brownianNoise (float x, float z)
+	{
 
 		float frequency = 1f;
 		float amplitude = .55f;
@@ -124,9 +127,8 @@ private float brownianNoise(float x, float z){
 		float total = 0f;
 		float lacunarity = 2f;
 
-		for (int i = 0; i < brownianOctaves; ++i)
-		{
-			total += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;         
+		for (int i = 0; i < brownianOctaves; ++i) {
+			total += Mathf.PerlinNoise (x * frequency, z * frequency) * amplitude;         
 			frequency *= lacunarity;
 			amplitude *= gain;
 		}
